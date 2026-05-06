324c7187b7b48517f86cbf8513471c8aae71e839
Lyndebbb <121833624+Lyndebbb@users.noreply.github.com>
2026-05-06 12:08:18 -0700

    Update index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>BLOOM</title>
+
+  <link rel="stylesheet" href="css/base.css">
+  <link rel="stylesheet" href="css/hero.css">
+  <link rel="stylesheet" href="css/data-section.css">
+  <link rel="stylesheet" href="css/closet-section.css">
+  <link rel="stylesheet" href="css/polar-section.css">
+</head>
+
+<body>
+
+  <?php include "components/hero.php"; ?>
+
+  <?php include "components/data-section.php"; ?>
+
+  <?php include "components/closet-section.php"; ?>
+
+  <?php include "components/polar-section.php"; ?>
+
+  <script src="js/main.js"></script>
+  <script src="js/data-section.js"></script>
+  <script src="js/closet-section.js"></script>
+  <script src="js/polar-section.js"></script>
+
+</body>
+</html>
